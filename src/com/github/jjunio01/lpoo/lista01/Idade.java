06705aa26f45774dab9f76373408c7323bc4bbd8
JJunio <junio.araujo01@yahoo.com>
2020-11-14 01:33:24 -0300

    Primeira questão. Calcular idade

--- src/com/github/jjunio01/lpoo/lista01/Idade.java
+++ src/com/github/jjunio01/lpoo/lista01/Idade.java
@@ -1,10 +1,41 @@
 package com.github.jjunio01.lpoo.lista01;
 
+import java.util.Scanner;
+
+/**
+ * @author JJunio
+ *
+ */
 public class Idade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Sua idade é: " + 
+				calcular(
+						lerInformações()
+				)
+		);
+		
+	}
+	
+	public static int[] lerInformações() {
+		
+		Scanner scan = new Scanner(System.in);
+		int anoAtual, anoNascimento;
+		
+		System.out.println("Digite o ano atual: ");
+		anoAtual = scan.nextInt();
+		System.out.println("Digite o ano de nascimento: ");
+		anoNascimento = scan.nextInt();
+		
+		int[] informacoes = {anoAtual, anoNascimento};
+		
+		return informacoes;
+	}
+	
+	public static int calcular(int[] datas) {
+		
+		return datas[0] - datas[1];
 	}
 
 }
