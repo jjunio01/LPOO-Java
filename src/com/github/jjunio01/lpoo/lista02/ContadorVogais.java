753892dff71cc11e3ab349c884a7a3bad47f359a
JJunio <junio.araujo01@yahoo.com>
2020-11-30 21:52:02 -0300

    Questao 9. Contador de vogais

---
+++ src/com/github/jjunio01/lpoo/lista02/ContadorVogais.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.github.jjunio01.lpoo.lista02;
+
+import java.util.Scanner;
+
+/**
+ * @author JJunio
+ *
+ */
+public class ContadorVogais {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int quantidadeVogais = 0;
+		String frase;
+		
+		System.out.println("Digite uma frase: ");
+		frase = scan.nextLine();
+		frase = frase.toLowerCase();
+		for (int i = 0; i < frase.length(); i++) {
+			
+			if (frase.charAt(i) == 'a' ||
+					frase.charAt(i) == 'e' ||
+						frase.charAt(i) == 'i' ||
+							frase.charAt(i) == 'o' ||
+								frase.charAt(i) == 'u') {
+				quantidadeVogais++;
+			}
+		}
+		
+		System.out.println("Na frase: " + frase);
+		System.out.println("Temos: " + quantidadeVogais + " vogais.");
+		
+	}
+
+}
