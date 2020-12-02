7526b58280bb59bfd5bd8c54100b224f4ec4c6e3
JJunio <junio.araujo01@yahoo.com>
2020-12-01 21:50:10 -0300

    Questão 5. Calcular área e perímetro do retângulo

---
+++ src/com/github/jjunio01/lpoo/lista03/TesteRetangulo.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.github.jjunio01.lpoo.lista03;
+
+import java.util.Scanner;
+
+/**
+ * @author JJunio
+ *
+ */
+public class TesteRetangulo {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Retangulo retangulo;
+		double base;
+		double altura;
+		double area;
+		double perimetro;
+		
+		System.out.println("Digite o valor da base do retângulo: ");
+		base = scan.nextDouble();
+		System.out.println("Digite o valor da altura do retângulo: ");
+		altura = scan.nextDouble();
+		retangulo = new Retangulo(base, altura);
+		area = retangulo.calcularArea();
+		perimetro = retangulo.calcularPerimetro();
+		
+		System.out.println("Retângulo: \n"
+				+ "Base: " + base
+				+ "\nAltura: " + altura
+				+ "\nArea: " + area
+				+ "\nPerimétro: " + perimetro );
+		
+		
+
+	}
+
+}
