7b76284a10f54f32a3dd85e0404e7cc6f3146288
JJunio <junio.araujo01@yahoo.com>
2020-12-09 21:22:12 -0300

    Questão 1. Funcionários com salário maior de 5k

---
+++ src/com/github/jjunio01/lpoo/lista04/Funcionario.java
@@ -0,0 +1,34 @@
+package com.github.jjunio01.lpoo.lista04;
+
+/**
+ * @author JJunio
+ *
+ */
+public class Funcionario {
+
+	private String nome;
+	private double salario;
+
+	public Funcionario(String nome, double salario) {
+
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+}
---
+++ src/com/github/jjunio01/lpoo/lista04/TesteSalario.java
@@ -0,0 +1,44 @@
+package com.github.jjunio01.lpoo.lista04;
+
+import javax.swing.JOptionPane;
+
+import com.github.jjunio01.lpoo.lista03.Pessoa;
+
+/**
+ * @author JJunio
+ *
+ */
+public class TesteSalario {
+
+	public static void main(String[] args) {
+
+		Funcionario[] listaFunci;
+		String nome;
+		double salario;
+
+		listaFunci = new Funcionario[Integer
+				.parseInt(JOptionPane.showInputDialog("Digite a quantidade de funcionários a serem cadastrados:"))
+
+		];
+		for (int i = 0; i < listaFunci.length; i++) {
+			nome = JOptionPane.showInputDialog("Digite o nome da " + (i + 1) + " pessoa.");
+			salario = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário da " + (i + 1) + " pessoa."));
+			listaFunci[i] = new Funcionario(nome, salario);
+		}
+
+		JOptionPane.showMessageDialog(null,
+				"Existem  : " + salarioMaiorCincoMil(listaFunci) + " pessoas com mais de 5.000,00.");
+	}
+
+	public static int salarioMaiorCincoMil(Funcionario[] funcionarios) {
+		int contador = 0;
+
+		for (Funcionario funci : funcionarios) {
+			if (funci.getSalario() > 5000) {
+				contador++;
+			}
+		}
+
+		return contador;
+	}
+}
